80dea91c61e1bc45c5018f010b76c9cb2e65e897
pmrugb <gbpmru@gmail.com>
2024-10-15 14:01:07 +0500

    fixes today 15th  oct by PMRU

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,67 +1,72 @@
 @extends('layouts.guest')
 @section('content')
-    @section('title')
-        {{ 'Log in' }}
-    @endsection
-    <div class="login-box">
-        <!-- /.login-logo -->
-        <div class="card card-outline card-primary">
-            <div class="card-header text-center">
-                <a href="/" class="h1"><b>{{ config('app.name') }}</a>
-            </div>
-            <div class="card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
-
-                <form action="{{ route('login') }}" method="POST">
-                    @csrf
-                    <div class="input-group mb-3">
-                        <input id="email" class="form-control @error('email') is-invalid @enderror" type="email" name="email" value="{{ old('email') }}"
-                            required autofocus autocomplete="username">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
-                        @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                    </div>
-                    <div class="input-group mb-3">
-                        <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" required
-                            autocomplete="current-password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                        @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" name="remember" id="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>                
-            </div>
-            <!-- /.card-body -->
-        </div>
-        <!-- /.card -->
-    </div>
+@section('title')
+{{ 'Log in' }}
 @endsection
+<div class="login-box">
+    <div class="login-logo">
+        <a href="/">
+            <img src="{{ asset('frontend/img/logo.png')}}" alt="Auth Logo" width="50" height="50">
+            <span class="text-center"><b>{{ config('app.name') }}</b></span>
+        </a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="card">
+        <div class="card-header bg-gradient-info">
+            <h3 class="card-title float-none text-center">
+                Sign in to start your session </h3>
+        </div>
+        <div class="card-body login-card-body ">
 
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="input-group mb-3">
+                    <input id="email" class="form-control @error('email') is-invalid @enderror" type="email" name="email" value="{{ old('email') }}"
+                        required autofocus autocomplete="username">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope fa-lg text-info"></span>
+                        </div>
+                    </div>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="input-group mb-3">
+                    <input id="password" class="form-control @error('password') is-invalid @enderror" type="password" name="password" required
+                        autocomplete="current-password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock fa-lg text-info"></span>
+                        </div>
+                    </div>
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="icheck-primary">
+                            <input type="checkbox" name="remember" id="remember">
+                            <label for="remember">
+                                Remember Me
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</div>
+@endsection
